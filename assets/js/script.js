10c13fc3efadee20867fe5e2fa72848ed66f2fb3
Cal Withington <scwithington@gmail.com>
2021-03-19 17:55:19 -0400

    defined variables for characters in password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+//   event.preventDefault();
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+    passwordText.value = password;
+    createPassword;
 
 }
 
+function generatePassword(length) {
+    var alpha = 'abcdefghijklmnopqrstuvwxyz';
+    var caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var int = '0123456789';
+    var spec = '!@#$%&*?';
+    var options = [alpha, caps, int, spec];
+    var password = '';
+    var passwordArray = Array(length);
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
