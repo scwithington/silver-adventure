12a07b2d9752379db400b3af37c1bb98af58b4de
Cal Withington <scwithington@gmail.com>
2021-03-20 14:33:16 -0400

    fixed confirms and alerts, attempting password randomization

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,15 @@ var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var caps = function(x) {return x.toUpperCase()};
 var int = ['0','1','2','3','4','5','6','7','8','9']
 var spec = ['!','@','#','$','%','&','*','?'];
+// var options = [confirmInt, confirmChar, confirmCaps, confirmAlpha];
 var options = [alpha, caps, int, spec];
-var password = '';
+var characters = '';
 var passwordArray = Array(length);
+
+var confirmInt;
+var confirmChar;
+var confirmCaps;
+var confirmAlpha;
 
 alpha2 = alpha.map(caps);
 // Write password to the #password input
@@ -28,8 +34,47 @@ function writePassword() {
             confirmChar = confirm('Will this contain special characters?');
             confirmCaps = confirm('Will this contain Uppercase letters?');
             confirmAlpha = confirm('Will this contain Lowercase letters?');
-        };
+        } 
+    }    
+    
+    if (!confirmInt, !confirmChar, !confirmCaps, !confirmAlpha) {
+        alert("Your password must contain at least one special, numeric, lowercase, or uppercase character.");
+        return generatePassword();
+    }
+    
+    
 
+    for(i = 1; i < length; i++) {
+        passwordText = passwordText + randomArray.charAt(Math.floor(Math.random() * Math.floor((characters.length) -1)));
+       
+    }  
+    if (confirmInt) {
+        characters.push(int);
+    }
+
+    if (confirmChar) {
+        characters.push(spec);
+        }
+    
+    if (confirmCaps) {
+        characters.push(caps);
+    }
+
+    if (confirmAlpha) {
+        characters.push(alpha);
+    }
+    document.getElementById("password").value = pass;
+
+    console.log(pass);
+       // if (confirmInt, confirmChar, confirmCaps, confirmAlpha) {  
+     //     var randomArray = character.concat(alpha2, int, spec, alpha);
+    // }
+    // 
+    // }
+    // console.log(randomize);    
+    
+
+        
 }
 
 // Add event listener to generate button
