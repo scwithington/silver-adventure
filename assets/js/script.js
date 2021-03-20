366534df9f179734b56e10cd14e1107ebdfb0840
Cal Withington <scwithington@gmail.com>
2021-03-20 16:19:37 -0400

    value of passwordText containing id #password set equal to finalPassword, comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
+
+// Array variables
 var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var caps = function(x) {return x.toUpperCase()};
 var int = ['0','1','2','3','4','5','6','7','8','9']
 var spec = ['!','@','#','$','%','&','*','?'];
-// var options = [confirmInt, confirmChar, confirmCaps, confirmAlpha];
-var options = [alpha, caps, int, spec];
 
-var characters = '';
-var passwordArray = Array(length);
-
+// Confirmation variables to be used in concatenation
 var confirmInt;
 var confirmChar;
 var confirmCaps;
 var confirmAlpha;
 
+// variable to call for alpha toUpper
 alpha2 = alpha.map(caps);
 
 generateBtn.addEventListener('click', writePassword); 
 // Write password to the #password input
 function writePassword() {
-//   event.preventDefault();
+    // local variables for password parameters.
     var passwordContainer = [];
     var finalPassword = '';
-    var length = 0;
+    var charSelect = 0;
     var password = generatePassword();
     var passwordText = document.querySelector('#password');
     passwordText.value = password;
-    // Assignment Code
+    // Generate password function to be run once button is pushed
     function generatePassword(){
-        var charSelect = prompt ('How many characters would you like?');
-        
+        charSelect = prompt ('How many characters would you like?');
+        // user prompted to state how many characters they would like in their password
+        // if statement assures 8 < length < 128
         if(!charSelect) {
             alert('This needs a value');
         } else if (charSelect < 8 || charSelect > 128){
             charSelect = alert('You must choose between 8 and 128');
             return;
         } else {
+            // confirmation of symbol types
             confirmInt = confirm('Will this contain numbers?');
             confirmChar = confirm('Will this contain special characters?');
             confirmCaps = confirm('Will this contain Uppercase letters?');
             confirmAlpha = confirm('Will this contain Lowercase letters?');
         } 
-        
+    
+    // if statements to concatenate array variables
     if (confirmInt) {
         passwordContainer = passwordContainer.concat(int);
     }
@@ -50,7 +52,7 @@ function writePassword() {
     if (confirmChar) {
         passwordContainer = passwordContainer.concat(spec);
     }
-    
+
     if (confirmCaps) {
         passwordContainer = passwordContainer.concat(alpha2);
     }
@@ -62,12 +64,14 @@ function writePassword() {
     // document.getElementById("password").value = passwordText;
 
    
-
+    // if statement preventing lack of selection
     if (!confirmInt, !confirmChar, !confirmCaps, !confirmAlpha) {
         alert("Your password must contain at least one special, numeric, lowercase, or uppercase character.");
         return generatePassword();
     } else randomizePassword();
 
+    /* for loop turning array into a string with proper length and setting 
+    finalPassword equal to the randomized selection*/
     function randomizePassword() {
         for(var i = 0; i < charSelect; i++) {
         var randomIdx = Math.floor(Math.random() * passwordContainer.length);
@@ -75,49 +79,9 @@ function writePassword() {
         finalPassword = randomChar + finalPassword;
         }  
     }}
-    
+
+    // logs randomized password in console
     console.log(finalPassword);
-     
-
-    
-       // if (confirmInt, confirmChar, confirmCaps, confirmAlpha) {  
-     //     var randomArray = character.concat(alpha2, int, spec, alpha);
-    // }
-    // 
-    // }
-    // console.log(randomize);    
-    
-        
+    // Pushes randomized password to appear on webpage
+    passwordText.value = finalPassword;    
 }
-
-// Add event listener to generate button
-   
-
-
-
-
-
-
-
-// console.log(answer);
-    // answer = parseInt (prompt ('How many characters would you like?'));
-    
-    // if(!answer) {
-    //     confirmInt = confirm('Will this contain numbers?');
-    //     confirmChar = confirm('Will this contain special characters?');
-    //     confirmCaps = confirm('Will this contain Uppercase letters?');
-    //     confirmAlpha = confirm('Will this contain Lowercase letters?');
-    // }
-
-    // function generatePassword(length) {
-    //     var alpha = 'abcdefghijklmnopqrstuvwxyz';
-    //     var caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    //     var int = '0123456789';
-    //     var spec = '!@#$%&*?';
-    //     var options = [alpha, caps, int, spec];
-    //     var password = '';
-    //     var passwordArray = Array(length);
-
-    //     for(i = 0; i < length; i++) {
-    //     }
-    // }
