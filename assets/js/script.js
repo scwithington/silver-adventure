f143ace55ea4963fc3b55e0e485093f18209b4a0
Cal Withington <scwithington@gmail.com>
2021-03-20 15:34:53 -0400

    randomize password function, console logging randomized password

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var int = ['0','1','2','3','4','5','6','7','8','9']
 var spec = ['!','@','#','$','%','&','*','?'];
 // var options = [confirmInt, confirmChar, confirmCaps, confirmAlpha];
 var options = [alpha, caps, int, spec];
+
 var characters = '';
 var passwordArray = Array(length);
 
@@ -15,9 +16,14 @@ var confirmCaps;
 var confirmAlpha;
 
 alpha2 = alpha.map(caps);
+
+generateBtn.addEventListener('click', writePassword); 
 // Write password to the #password input
 function writePassword() {
 //   event.preventDefault();
+    var passwordContainer = [];
+    var finalPassword = '';
+    var length = 0;
     var password = generatePassword();
     var passwordText = document.querySelector('#password');
     passwordText.value = password;
@@ -29,43 +35,51 @@ function writePassword() {
             alert('This needs a value');
         } else if (charSelect < 8 || charSelect > 128){
             charSelect = alert('You must choose between 8 and 128');
+            return;
         } else {
             confirmInt = confirm('Will this contain numbers?');
             confirmChar = confirm('Will this contain special characters?');
             confirmCaps = confirm('Will this contain Uppercase letters?');
             confirmAlpha = confirm('Will this contain Lowercase letters?');
         } 
-    }    
-    
-    if (!confirmInt, !confirmChar, !confirmCaps, !confirmAlpha) {
-        alert("Your password must contain at least one special, numeric, lowercase, or uppercase character.");
-        return generatePassword();
-    }
-    
-    
-
-    for(i = 1; i < length; i++) {
-        passwordText = passwordText + randomArray.charAt(Math.floor(Math.random() * Math.floor((characters.length) -1)));
-       
-    }  
+        
     if (confirmInt) {
-        characters.push(int);
+        passwordContainer = passwordContainer.concat(int);
     }
 
     if (confirmChar) {
-        characters.push(spec);
-        }
+        passwordContainer = passwordContainer.concat(spec);
+    }
     
     if (confirmCaps) {
-        characters.push(caps);
+        passwordContainer = passwordContainer.concat(alpha2);
     }
 
     if (confirmAlpha) {
-        characters.push(alpha);
+        passwordContainer = passwordContainer.concat(alpha);
     }
-    document.getElementById("password").value = pass;
 
-    console.log(pass);
+    // document.getElementById("password").value = passwordText;
+
+   
+
+    if (!confirmInt, !confirmChar, !confirmCaps, !confirmAlpha) {
+        alert("Your password must contain at least one special, numeric, lowercase, or uppercase character.");
+        return generatePassword();
+    } else randomizePassword();
+
+    function randomizePassword() {
+        for(var i = 0; i < charSelect; i++) {
+        var randomIdx = Math.floor(Math.random() * passwordContainer.length);
+        var randomChar = passwordContainer[randomIdx];
+        finalPassword = randomChar + finalPassword;
+        }  
+    }}
+    
+    console.log(finalPassword);
+     
+
+    
        // if (confirmInt, confirmChar, confirmCaps, confirmAlpha) {  
      //     var randomArray = character.concat(alpha2, int, spec, alpha);
     // }
@@ -73,12 +87,11 @@ function writePassword() {
     // }
     // console.log(randomize);    
     
-
         
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);    
+   
 
 
 
